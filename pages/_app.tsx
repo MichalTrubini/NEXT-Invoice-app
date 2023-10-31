3b66c63519401edd6fe14d85ec0bac754e1fae94
Michal Trubini <mchl.trubini@gmail.com>
2023-10-31 21:59:44 +0100

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../src/styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../src/layout/Layout";
 import { SiteProvider } from "../src/store/site-context";
+import Layout from "../src/layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
