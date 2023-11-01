b0406c0f915985ea898cde6b4cf0379f91392dff
Michal Trubini <mchl.trubini@gmail.com>
2023-11-01 12:48:04 +0100

    puffLoader

--- src/components/loadingSpinner.tsx
+++ src/components/loadingSpinner.tsx
@@ -1,4 +1,4 @@
-import { PulseLoader } from "react-spinners";
+import { PuffLoader } from "react-spinners";
 import styles from "./loadingSpinner.module.css";
 import { useContext } from "react";
 import { SiteContext } from "store/site-context";
@@ -10,7 +10,7 @@ const LoadingSpinner = () => {
   return (
     <div className={styles.loadingSpinner}>
       <div className={styles.overlay}></div>
-      <PulseLoader color={setThemeStyles('spinner') === 'spinner-light' ? '#0c0e16' : '#fff'} loading={true} size={120} />
+      <PuffLoader color={setThemeStyles('spinner') === 'spinner-light' ? '#0c0e16' : '#fff'} loading={true} size={120} />
     </div>
   );
 };
